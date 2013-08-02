a697962012104eaca951615c5d187feb212edb32
Alberto Dubois <aduboisr@gmail.com>
2013-08-02 16:22:36 -0700

    Initial commit

---
+++ jovel/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jovel::Application.config.secret_key_base = 'aa75c8a7be1b0fd0ff0906f8a41f7855c3af0588e98d186487cee99982055debe45ac2260c6011d5979c5459d4439d31d93075e6dacc4e800ebb94e368aa8f19'
---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'd26fd315e421e841fdba53f1a5dfb548f57137b73ec8c24903942926a8cd562b8974d385b7d0e2dcaf624478454381b54756e436f7e684b258f204607c921581'
